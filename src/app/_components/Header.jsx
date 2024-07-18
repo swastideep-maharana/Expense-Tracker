cefffd2c6827c65f037a02081761c982e4bc9411
swastideep maharana <swastideep67maharana@gmail.com>
2024-07-18 15:13:07 +0530

    #header

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -3,3 +3,22 @@
 import React from "react";
 import Image from "next/image";
 import Button from "@/components/ui/button";
+import { useUser, UserButton } from "@clerk/nextjs";
+
+function Header() {
+  const { user, isSignedIn } = useUser();
+  return (
+    <div className="p-5 flex justify-center items-center border shadow-sm">
+      <div>
+        {/* logo */}
+        <span>finance smart</span>
+      </div>
+      <div>
+        <button>button 1</button>
+        <button>button 2</button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
