587840155499a825cd2c40afe3eaddc6e6698da6
swastideep maharana <swastideep67maharana@gmail.com>
2024-07-18 18:09:46 +0530

    #navbar

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -2,21 +2,37 @@
 
 import React from "react";
 import Image from "next/image";
-import Button from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { useUser, UserButton } from "@clerk/nextjs";
 
 function Header() {
   const { user, isSignedIn } = useUser();
   return (
-    <div className="p-5 flex justify-center items-center border shadow-sm">
-      <div>
-        {/* logo */}
-        <span>finance smart</span>
+    <div className="p-5 flex justify-between items-center border shadow-sm">
+      <div className="flex flex-row items-center">
+        <Image
+          src="/chart-donut.svg"
+          alt="Finance Smart Logo"
+          width={40}
+          height={25}
+        />
+        <span className="text-blue-800 font-bold text-xl">FinanceSmart</span>
       </div>
-      <div>
-        <button>button 1</button>
-        <button>button 2</button>
-      </div>
+      {isSignedIn ? (
+        <UserButton />
+      ) : (
+        <div className="flex gap-3 items-center">
+          <Link href="/dashboard">
+            <Button variant="outline" className="rounded-full">
+              Dashboard
+            </Button>
+          </Link>
+          <Link href="/dashboard">
+            <Button className="rounded-full">Get Started</Button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
